ad7b95108ec15319711fa40c8cbca68fd2a91edd
Yuseon Lee <usun1128@naver.com>
2022-05-01 04:58:47 +0900

    character commit

--- pygame_application/Avoid_falling_poo.py
+++ pygame_application/Avoid_falling_poo.py
@@ -15,6 +15,18 @@ clock = pygame.time.Clock()
 # 배경 만들기
 back = pygame.image.load("C:\\Pygame_application\\pygame_application\\background.png")
 
+# 캐릭터 만들기
+char = pygame.image.load('C:\\Pygame_application\\pygame_application\\character.png')
+char_size = char.get_rect().size
+char_width = char_size[0]
+char_height = char_size[1]
+char_x_pos = (screen_width / 2) - (char_width / 2)
+char_y_pos = screen_height - char_height
+
+# 이동 위치
+to_x = 0
+
+char_speed = 1
 
 running = True
 while running:
@@ -24,10 +36,29 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                to_x -= char_speed
+            elif event.key == pygame.K_RIGHT:
+                to_x += char_speed
+           
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                to_x = 0
+
+# 게임 캐릭터 위치 정의
+    char_x_pos += to_x * dt
+
+    if char_x_pos < 0:
+        char_x_pos = 0
+    elif char_x_pos > screen_width - char_width:
+        char_x_pos = screen_width - char_width
 
 
 # 화면에 그리기
     screen.blit(back, (0,0))
+    screen.blit(char, (char_x_pos, char_y_pos))
 
 
     pygame.display.update()
